4897d8d390cc17626bcd4000a6fce5f53d231e82
Rajat504 <rajatravindar504@gmail.com>
2020-01-23 12:23:54 +0530

    [Rajat] add . adding shell scripting

---
+++ Hnm.sh
@@ -0,0 +1,9 @@
+echo "Hello"
+read -p "Please enter your names:"name
+echo "$name>>name.txt
+clear
+echo -e "Hello $name \your name has been added to the list"
+echo "you got 1"
+cat name.txt
+echo "Goodbye $name"
+sleep 2
